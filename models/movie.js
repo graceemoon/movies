376cc21321455fe217cbbc5dfb4eddfc657b9e2c
grace moon <hi.gracemoon@gmail.com>
2016-11-08 16:25:44 -0500

    get put delete no errors

--- models/movie.js
+++ models/movie.js
@@ -10,29 +10,76 @@ const offset = 0;
 
 function getAllMovies(req, res, next) {
 // implement get all movies
+	db.any(`SELECT * FROM movies ORDER BY id LIMIT ${limit} OFFSET ${offset};`)
+	.then((movies) => {
+		res.rows= movies;
+		next();
+	})
+	.catch(err => next(err));
 }
 
 function getMovie(req, res, next) {
 // implement get single movie
+	const movieID = parseInt(req.params.id);
+	db.one(`SELECT * FROM movies WHERE id = $1`, movieID)
+	.then ((oneMovie) => {
+		res.rows = oneMovie;
+		next();
+	})
+	.catch(err => next(err));
 }
 
 function updateMovie(req, res, next) {
 // implement update
+	req.body.ID = Number.parseInt(req.params.id);
+	db.none(`UPDATE movies 
+		SET title=$2 
+		WHERE id=$1;`, 
+		[req.body.ID, req.body.title])
+	.then(function () {
+		res.status(200)
+		.json({
+			status: 'success',
+			message: 'update'
+		});
+	})
+	.catch(err => next(err));
 }
 
-function deletemovie(req, res, next) {
+function deleteMovie(req, res, next) {
 // implement delete
+// 	deleteID = Number.parseInt(req.params.id);
+// 	db.none(`DELETE FROM movies WHERE id=$1`, [deleteID])
+// 	.then (function() {
+// 		res.status(200)
+// 		.json({
+// 			status: 'success',
+// 			message: 'update'
+// 		});
+// 	})
+// 	.catch(err => next(err));
+// }
+	const dID = parseInt(req.params.id);
+	db.result(`DELETE FROM movies WHERE id=$1`, [dID])
+	.then (function (result){
+		res.status(200)
+		.json({
+			status: 'success',
+			message: 'deleted'
+		});
+	})
+	.catch(err => next(err));
 }
 
-// BONUS
-function getAllMoviesWithRatings(req, res, next) {
+// // BONUS
+// function getAllMoviesWithRatings(req, res, next) {
 
-}
+// }
 
 module.exports = {
   getAllMovies,
   getMovie,
   updateMovie,
-  deletemovie,
-  getAllMoviesWithRatings
+  deleteMovie,
+  // getAllMoviesWithRatings
 };
--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { getAllMovies, getMovie, getAllMoviesWithRatings } = require('../../models/movie');
+const { getAllMovies, getMovie, getAllMoviesWithRatings, updateMovie, deleteMovie } = require('../../models/movie');
+
+
 
 // handle all the routes
+const sendJSONresp = (req, res) => res.json(res.rows);
 
 // get all movies
+router.route('/')
+	.get(getAllMovies, sendJSONresp)
+	.put()
+	.delete();
 
-// Get movies withrating BONUS
+
+// // Get movies withrating BONUS
+// router.route('/ratings')
+// 	.get(getAllMoviesWithRatings, sendJSONresp)
+// 	.put()
+// 	.delete();
+
 
 // Get single movie
+router.route('/:id')
+	.get(getMovie, sendJSONresp)
+	.put(updateMovie)
+	.delete(deleteMovie);
+
 
 module.exports = router;
